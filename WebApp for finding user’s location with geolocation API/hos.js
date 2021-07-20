e4c0c9eb2d2e934f24550c690b11249e75f39897
akbar012 <87679116+akbar012@users.noreply.github.com>
2021-07-19 20:57:59 -0400

    Add files via upload

---
+++ WebApp for finding user’s location with geolocation API/hos.js
@@ -0,0 +1,101 @@
+var hos = [
+  {
+    "X": 593646,
+    "Y": 4788124,
+    "OBJECTID": 1,
+    "NAME": "Juravinski Cancer Centre",
+    "ADDRESS": "699 Concession Street",
+    "COMMUNITY": "Hamilton",
+    "TYPE": "",
+    "LONGITUDE": -79.84659072801017,
+    "LATITUDE": 43.24010159219824
+  },
+  {
+    "X": 587923,
+    "Y": 4787876,
+    "OBJECTID": 2,
+    "NAME": "Chedoke Hospital",
+    "ADDRESS": "565 Sanatorium Road",
+    "COMMUNITY": "Hamilton",
+    "TYPE": "",
+    "LONGITUDE": -79.91710597305843,
+    "LATITUDE": 43.238557763378495
+  },
+  {
+    "X": 592984,
+    "Y": 4790541,
+    "OBJECTID": 3,
+    "NAME": "Hamilton General Hospital",
+    "ADDRESS": "237 Barton Street East",
+    "COMMUNITY": "Hamilton",
+    "TYPE": "",
+    "LONGITUDE": -79.85433493263169,
+    "LATITUDE": 43.26194349249136
+  },
+  {
+    "X": 593775,
+    "Y": 4788129,
+    "OBJECTID": 4,
+    "NAME": "Juravinski Hospital",
+    "ADDRESS": "711 Concession Street",
+    "COMMUNITY": "Hamilton",
+    "TYPE": "",
+    "LONGITUDE": -79.84500134029395,
+    "LATITUDE": 43.24013057720681
+  },
+  {
+    "X": 587850,
+    "Y": 4790214,
+    "OBJECTID": 5,
+    "NAME": "McMaster University Medical Centre",
+    "ADDRESS": "1200 Main Street West",
+    "COMMUNITY": "Hamilton",
+    "TYPE": "",
+    "LONGITUDE": -79.9176322156679,
+    "LATITUDE": 43.2596154898273
+  },
+  {
+    "X": 591658,
+    "Y": 4789041,
+    "OBJECTID": 6,
+    "NAME": "St. Joseph's Hospital - Charlton Campus",
+    "ADDRESS": "50 Charlton Avenue East",
+    "COMMUNITY": "Hamilton",
+    "TYPE": "",
+    "LONGITUDE": -79.87091912976558,
+    "LATITUDE": 43.24860153779506
+  },
+  {
+    "X": 594428,
+    "Y": 4788652,
+    "OBJECTID": 7,
+    "NAME": "St. Peter's Hospital",
+    "ADDRESS": "88 Maplewood Avenue",
+    "COMMUNITY": "Hamilton",
+    "TYPE": "Chronic Care",
+    "LONGITUDE": -79.83687057239422,
+    "LATITUDE": 43.2447575877267
+  },
+  {
+    "X": 590677,
+    "Y": 4788332,
+    "OBJECTID": 8,
+    "NAME": "St. Joseph's Centre for Mountain Health Services - West 5th Campus",
+    "ADDRESS": "100 West 5th Street",
+    "COMMUNITY": "Hamilton",
+    "TYPE": "Psychiatric",
+    "LONGITUDE": -79.8831178614224,
+    "LATITUDE": 43.24233704460304
+  },
+  {
+    "X": 599582,
+    "Y": 4786167,
+    "OBJECTID": 9,
+    "NAME": "St. Joseph's Hospital - Urgent Care at King Campus",
+    "ADDRESS": "2757 King Street East",
+    "COMMUNITY": "Hamilton",
+    "TYPE": "Urgent Care",
+    "LONGITUDE": -79.77384804381661,
+    "LATITUDE": 43.22172294601865
+  }
+]
